b92e57b521471c2ed6ef87cd91883ddcde4b1b09
Daniel Rojas <l20j45_@gmail.com>
2025-11-09 10:57:27 -0600

    Hasta aqui estan los productos checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
+  // AÑADE ESTA LÍNEA
+  // Le dice a Astro/Vite que todas las rutas estáticas deben usar este prefijo.
   integrations: [svelte({ extensions: ['.svelte'] })],
 });
--- src/components/BootsList.svelte
+++ src/components/BootsList.svelte
@@ -1,12 +1,16 @@
 <script>
     import { bootsData } from "../data/bootsData.js";
+    import Modal from "./Modal.svelte";
 
     let searchTerm = "";
     let selectedCategory = "";
     let selectedFeatures = [];
     let showAll = false;
+    
+    // Variables para el modal
+    let isModalOpen = false;
+    let selectedBoot = null;
 
-    // Lista de características disponibles
     const featuresList = [
         "Puntera de acero",
         "Antideslizante",
@@ -14,7 +18,6 @@
         "Dieléctrico",
     ];
 
-    // Filtrado reactivo
     $: filteredBoots = bootsData.filter((boot) => {
         const nameMatch = boot.Modelo.toLowerCase().includes(
             searchTerm.toLowerCase(),
@@ -46,6 +49,16 @@
 
     function toggleShowAll() {
         showAll = !showAll;
+    }
+    
+    function openModal(boot) {
+        selectedBoot = boot;
+        isModalOpen = true;
+    }
+    
+    function closeModal() {
+        isModalOpen = false;
+        selectedBoot = null;
     }
 </script>
 
@@ -111,49 +124,49 @@
 <!-- Productos -->
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
     {#each bootsToShow as boot}
-<div class="group border border-gray-900 hover:border-white rounded-3xl overflow-hidden transition duration-300">
-              <div class="relative overflow-hidden bg-gray-900 bg-opacity-30">
-                <img class="w-full h-64 object-cover transform group-hover:scale-105 transition duration-500" src="images/Mod-2048-musgo.jpg" alt="Bota de Seguridad Industrial">
+        <div class="group border border-gray-900 hover:border-white rounded-3xl overflow-hidden transition duration-300">
+            <div class="relative overflow-hidden bg-gray-900 bg-opacity-30">
+                <img class="w-full h-64 object-cover transform group-hover:scale-105 transition duration-500" src="/images/Mod-2048-musgo.jpg" alt="Bota de Seguridad Industrial">
                 <div class="absolute top-4 right-4">
                     {#if boot?.new == "yes"}
-                  <span class="px-3 py-1.5 text-sm font-medium text-center text-green-400 uppercase border border-green-400 rounded-full bg-black bg-opacity-50">Nuevo</span>
-                  {/if}
+                        <span class="px-3 py-1.5 text-sm font-medium text-center text-green-400 uppercase border border-green-400 rounded-full bg-black bg-opacity-50">Nuevo</span>
+                    {/if}
                 </div>
-              </div>
-              <div class="p-8">
+            </div>
+            <div class="p-8">
                 <h4 class="mb-3 text-2xl text-black tracking-tighter">Modelo: {boot.Modelo}</h4>
                 <p class="mb-4 text-black text-opacity-60 text-sm">{boot.Descripcion_General}</p>
                 <div class="mb-4 space-y-2">
-
-                                        {#each boot.Beneficios.slice(0, 3) as b}
-                  <div class="flex items-center text-black text-opacity-60 text-sm">
-                    <svg class="mr-2 text-green-400" width="16" height="16" viewbox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M6.66667 10.6667L4 8.00004L3.06 8.94004L6.66667 12.5467L14 5.21337L13.06 4.27337L6.66667 10.6667Z" fill="currentColor"></path>
-                    </svg>
-                    <span>{b}</span>
-                  </div>
+                    {#each boot.Beneficios.slice(0, 3) as b}
+                        <div class="flex items-center text-black text-opacity-60 text-sm">
+                            <svg class="mr-2 text-green-400" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                <path d="M6.66667 10.6667L4 8.00004L3.06 8.94004L6.66667 12.5467L14 5.21337L13.06 4.27337L6.66667 10.6667Z" fill="currentColor"></path>
+                            </svg>
+                            <span>{b}</span>
+                        </div>
                     {/each}
-                                    {#if boot.Beneficios.length > 3}
-                    <p class="text-sm text-gray-500">
-                        + {boot.Beneficios.length - 3} beneficios más
-                    </p>
-                {/if}
-
-
+                    {#if boot.Beneficios.length > 3}
+                        <p class="text-sm text-gray-500">
+                            + {boot.Beneficios.length - 3} beneficios más
+                        </p>
+                    {/if}
                 </div>
                 <div class="flex items-center justify-between mb-4">
-
-                  <a class="inline-block px-10 py-2 text-black font-medium rounded-full bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-500 focus:ring-opacity-40 transition duration-300" href="https://wa.me/message/YO45MCDXZ6ILG1">Contactar</a>
+                    <a class="inline-block px-10 py-2 text-black font-medium rounded-full bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-500 focus:ring-opacity-40 transition duration-300" href="https://wa.me/message/YO45MCDXZ6ILG1">Contactar</a>
                 </div>
-                <a class="group inline-flex items-center" href="#">
-                  <span class="mr-3.5 text-black font-medium underline">Ver detalles</span>
-                  <svg class="transform group-hover:rotate-90 transition duration-300" width="13" height="12" viewbox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.5 0.75L1 11.25" stroke="black" stroke-width="1.43182" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M11.5 10.3781V0.75H1.87187" stroke="black" stroke-width="1.43182" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                  </svg>
-                </a>
-              </div>
+                <button 
+                    class="group inline-flex items-center w-full cursor-pointer" 
+                    on:click={() => openModal(boot)}
+                    type="button"
+                >
+                    <span class="mr-3.5 text-black font-medium underline">Ver detalles</span>
+                    <svg class="transform group-hover:rotate-90 transition duration-300" width="13" height="12" viewBox="0 0 13 12" fill="none">
+                        <path d="M11.5 0.75L1 11.25" stroke="black" stroke-width="1.43182" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M11.5 10.3781V0.75H1.87187" stroke="black" stroke-width="1.43182" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </button>
             </div>
+        </div>
     {/each}
 </div>
 
@@ -170,3 +183,10 @@
         </button>
     </div>
 {/if}
+
+<!-- Modal -->
+<Modal 
+    isOpen={isModalOpen} 
+    boot={selectedBoot} 
+    onClose={closeModal}
+/>
